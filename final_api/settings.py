662de52b3342fc7461dd004162e6fac390bce045
ulpa11 <58286940+ulpa11@users.noreply.github.com>
2021-05-14 23:12:11 +0530

    Add files via upload

--- final_api/settings.py
+++ final_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9+!4a7hk**4l+p)+_d(r6=3#v4b9$noswmu0n4%^!n()4iqxbt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["finalapi11.herokuapp.com/",'127.0.0.1:8000','127.0.0.1']
+ALLOWED_HOSTS = ["finalapi11.herokuapp.com",'127.0.0.1:8000','127.0.0.1']
 
 
 # Application definition
